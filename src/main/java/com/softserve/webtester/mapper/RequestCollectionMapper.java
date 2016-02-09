c59c55fd59f543215f18e2d594555feb37499ee1
Yura Lubinec <yuralubinec@gmail.com>
2016-02-09 23:37:16 +0200

    add CollectionRequest model and mapper

---
+++ src/main/java/com/softserve/webtester/mapper/RequestCollectionMapper.java
@@ -0,0 +1,46 @@
+package com.softserve.webtester.mapper;
+
+import java.util.LinkedHashSet;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+import org.apache.ibatis.type.JdbcType;
+import com.softserve.webtester.model.Request;
+import com.softserve.webtester.model.RequestCollection;
+
+@Repository
+public interface RequestCollectionMapper {
+	
+    @Insert("INSERT INTO RequestCollection (name, description) VALUES (#{name}, #{description})")  
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int save(RequestCollection RequestCollection);
+	
+	@Select("SELECT*FROM RequestCollection")
+	@Results(value={
+			@Result(property = "id", column = "ID", jdbcType = JdbcType.BIGINT),
+			@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
+			@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR)			
+	})
+	LinkedHashSet<RequestCollection> loadAll();
+	
+	@Select("SELECT*FROM RequestCollection WHERE id = #{id}")
+	@Results(value={
+			@Result(property = "id", column = "id", jdbcType = JdbcType.BIGINT),
+			@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
+			@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR),
+			@Result(property = "requests", column ="id", javaType = LinkedHashSet.class,
+					many = @Many(select = "com.softserve.webtester.mapper.RequestMapper.loadByRequestCollectionId"))           
+	})
+	RequestCollection loadByCollectionId(long id);
+	
+	@Update("UPDATE RequestCollection SET name = #{name}, description = #{description}, WHERE id = #{id}")
+	int update(RequestCollection requestCollection);
+	
+	@Delete("DELETE FROM RequestCollection WHERE id = #{id}")
+	int detele(long id);
+	
+	@Delete("DELETE FROM RequestCollection_Request WHERE requestId = #{id}")
+    int deleteByRequestId(long id);
+	
+	
+
+}
