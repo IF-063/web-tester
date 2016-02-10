1ef08ceafac736459195ae6188925692123c8618
Yura Lubinec <yuralubinec@gmail.com>
2016-02-10 19:52:24 +0200

    fixed deleteFromCollection method in mapper

--- src/main/java/com/softserve/webtester/mapper/RequestCollectionMapper.java
+++ src/main/java/com/softserve/webtester/mapper/RequestCollectionMapper.java
@@ -14,32 +14,30 @@ public interface RequestCollectionMapper {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     int save(RequestCollection RequestCollection);
 	
-	@Select("SELECT*FROM RequestCollection")
-	@Results(value={
-			@Result(property = "id", column = "ID", jdbcType = JdbcType.BIGINT),
-			@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
-			@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR)			
-	})
-	LinkedHashSet<RequestCollection> loadAll();
+    @Select("SELECT*FROM RequestCollection")
+    @Results({	@Result(property = "id", column = "ID", jdbcType = JdbcType.INTEGER),
+		@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
+		@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR)			
+    })
+    LinkedHashSet<RequestCollection> loadAll();
 	
-	@Select("SELECT*FROM RequestCollection WHERE id = #{id}")
-	@Results(value={
-			@Result(property = "id", column = "id", jdbcType = JdbcType.BIGINT),
-			@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
-			@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR),
-			@Result(property = "requests", column ="id", javaType = LinkedHashSet.class,
-					many = @Many(select = "com.softserve.webtester.mapper.RequestMapper.loadByRequestCollectionId"))           
-	})
-	RequestCollection loadByCollectionId(long id);
+    @Select("SELECT*FROM RequestCollection WHERE id = #{id}")
+    @Results({	@Result(property = "id", column = "id", jdbcType = JdbcType.INTEGER),
+		@Result(property = "name", column = "name", jdbcType = JdbcType.VARCHAR),
+		@Result(property = "description", column = "description", jdbcType = JdbcType.VARCHAR),
+		@Result(property = "requests", column ="id", javaType = LinkedHashSet.class,
+			many = @Many(select = "com.softserve.webtester.mapper.RequestMapper.loadByRequestCollectionId"))           
+    })
+    RequestCollection load(int id);
 	
-	@Update("UPDATE RequestCollection SET name = #{name}, description = #{description}, WHERE id = #{id}")
-	int update(RequestCollection requestCollection);
+    @Update("UPDATE RequestCollection SET name = #{name}, description = #{description}, WHERE id = #{id}")
+    int update(RequestCollection requestCollection);
 	
-	@Delete("DELETE FROM RequestCollection WHERE id = #{id}")
-	int detele(long id);
+    @Delete("DELETE FROM RequestCollection WHERE id = #{id}")
+    int detele(int id);
 	
-	@Delete("DELETE FROM RequestCollection_Request WHERE requestId = #{id}")
-    int deleteByRequestId(long id);
+    @Delete("DELETE FROM RequestCollection_Request WHERE requestId = #{rId} and requestCollectionId = #{rcId}")
+    int deleteFromCollection(int rId, int rcId);
 	
 	
 
