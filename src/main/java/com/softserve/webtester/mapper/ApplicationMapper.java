a670f14ce02d2030642b9c65b5a954a416aa1b74
Roman Zolotar <zolotar.box@gmail.com>
2016-02-11 16:44:18 +0200

    App and Serv models added

---
+++ src/main/java/com/softserve/webtester/mapper/ApplicationMapper.java
@@ -0,0 +1,5 @@
+package com.softserve.webtester.mapper;
+
+public interface ApplicationMapper {
+
+}
---
+++ src/main/java/com/softserve/webtester/model/Service.java
@@ -0,0 +1,55 @@
+package com.softserve.webtester.model;
+
+import java.io.Serializable;
+
+/**
+ * The Service class represents {@code Label} entity stored in the database.
+ *
+ * @author Roman Zolotar
+ * @version 1.0
+ */
+
+public class Service implements Serializable {
+	
+	private int id;
+	private String name;
+	private String description;
+	private byte deleted;
+	
+	public Service() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public byte getDeleted() {
+		return deleted;
+	}
+
+	public void setDeleted(byte deleted) {
+		this.deleted = deleted;
+	}
+
+}
+
