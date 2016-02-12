ed89d492d29a613b2ae817906d91fe212a749a20
Roman Zolotar <zolotar.box@gmail.com>
2016-02-12 11:22:46 +0200

    Application&Service mappers fixed

--- src/main/java/com/softserve/webtester/mapper/ServiceMapper.java
+++ src/main/java/com/softserve/webtester/mapper/ServiceMapper.java
@@ -1,41 +1,44 @@
 package com.softserve.webtester.mapper;
 
-import java.util.List;
+import java.util.LinkedHashSet;
 
 import org.apache.ibatis.annotations.*;
+
 import com.softserve.webtester.model.Service;
 
 /**
  * ServiceMapper is MyBatis mapper interface for CRUD operations.
  * @author Roman Zolotar
- * @version 1.0
+ * @version 1.1
  */
 
 public interface ServiceMapper {
-	final String getAll = "SELECT * FROM Application";
-	final String getById = "SELECT * FROM Application WHERE ID = #{id}";
-	final String deleteById = "DELETE from Application WHERE ID = #{id}";
+	final String loadAll = "SELECT * FROM Application";
+	final String load = "SELECT * FROM Application WHERE ID = #{id}";
+	final String delete = "DELETE from Application WHERE ID = #{id}";
 	final String insert = "INSERT INTO Application (NAME, DESCRIPTION, DELETED) VALUES (#{name}, #{description}, #{deleted})";
 	final String update = "UPDATE Application SET DELETED = #{deleted}, NAME = #{name}, DESCRIPTION = #{description} WHERE ID = #{id}";
 
-	@Select(getAll)
-	@Results(value = { @Result(property = "id", column = "ID"), @Result(property = "name", column = "NAME"),
-			@Result(property = "description", column = "DESCRIPTION"),
-			@Result(property = "deleted", column = "DELETED") })
+	@Select(loadAll)
+	@Results(value = { 	@Result(property = "id", column = "ID"), 
+						@Result(property = "name", column = "NAME"),
+						@Result(property = "description", column = "DESCRIPTION"),
+						@Result(property = "deleted", column = "DELETED") })
 
-	List getAll();
+	LinkedHashSet<Service> loadAll();
 
-	@Select(getById)
-	@Results(value = { @Result(property = "id", column = "ID"), @Result(property = "name", column = "NAME"),
-			@Result(property = "description", column = "DESCRIPTION"),
-			@Result(property = "deleted", column = "DELETED") })
+	@Select(load)
+	@Results(value = { 	@Result(property = "id", column = "ID"), 
+						@Result(property = "name", column = "NAME"),
+						@Result(property = "description", column = "DESCRIPTION"),
+						@Result(property = "deleted", column = "DELETED") })
 
-	Service getById(int id);
+	Service load(int id);
 
 	@Update(update)
 	void update(Service service);
 
-	@Delete(deleteById)
+	@Delete(delete)
 	void delete(int id);
 
 	@Insert(insert)
