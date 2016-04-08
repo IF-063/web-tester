d761cfc8cbf068a1d93c9ea1020b1cb5bdccb8c2
Roman Zolotar <zolotar.box@gmail.com>
2016-04-08 18:15:18 +0300

    Some changes in ParseAndWriteService according to code-review

--- src/main/java/com/softserve/webtester/service/ParseAndWriteService.java
+++ src/main/java/com/softserve/webtester/service/ParseAndWriteService.java
@@ -28,6 +28,7 @@ import com.softserve.webtester.model.HeaderHistory;
 import com.softserve.webtester.model.Label;
 import com.softserve.webtester.model.Request;
 import com.softserve.webtester.model.RequestCollection;
+import com.softserve.webtester.model.RequestMethod;
 import com.softserve.webtester.model.ResultHistory;
 
 // TODO RZ: JavaDoc
@@ -53,10 +54,8 @@ public class ParseAndWriteService {
     public int parseAndWrite(ResultsDTO resultsDTO) {
 
         int runId = resultsDTO.getRunId();
-        Environment environment = resultsDTO.getEnvironment();
-        int buildVersionId = resultsDTO.getBuildVersionId();
-        List<CollectionResultDTO> collectionResultDTOList = resultsDTO.getCollectionResultDTOList();
 
+        List<CollectionResultDTO> collectionResultDTOList = resultsDTO.getCollectionResultDTOList();
         ResultHistory resultHistory = new ResultHistory();
         EnvironmentHistory environmentHistory = new EnvironmentHistory();
         HeaderHistory headerHistory = new HeaderHistory();
@@ -71,168 +70,199 @@ public class ParseAndWriteService {
                 RequestDTO requestDTO = requestResult.getRequestDTO();
                 List<ResponseDTO> responseDTOList = requestResult.getResponses();
 
-                boolean statusIndicator = false;
+                savingResultHistory(request, resultHistory, requestDTO, resultsDTO, responseDTOList, collectionId,
+                        dbValidationHistory);
+                savingEnvironmentHistory(environmentHistory, resultHistory, resultsDTO);
+                savingHeaderHistory(request, headerHistory, resultHistory);
+                savingLabelHistory(request, resultHistory);
+            }
+        }
+        return runId;
+    }
 
-                if (buildVersionId != 0) {
-                    int count = 0;
-                    int timeSum = 0;
-                    for (ResponseDTO responseDTOListElement : responseDTOList) {
-                        if (responseDTOListElement.getStatusCode() == 200 && true /*TODO AM: check if actual response is equal to expected + other validation*/) { // TODO RZ: move to constants
-                            timeSum += responseDTOListElement.getResponseTime();
-                            count++;
-                            resultHistory.setStatusLine(responseDTOListElement.getStatusLine());
-                            resultHistory.setActualResponse(
-                                    requestExecuteSupportService.format(responseDTOListElement.getResponseBody()));
-                        }
-                        resultHistory.setResponseTime(timeSum/count);
-                        statusIndicator = (count == 5); // TODO RZ: move to constants use from property file, which Anton should extract
-                    }
-                } else {
-                    ResponseDTO responseDTO = responseDTOList.get(0);
-                    resultHistory.setResponseTime(responseDTO.getResponseTime());
-                    resultHistory.setStatusLine(responseDTO.getStatusLine());
-                    resultHistory.setActualResponse(requestExecuteSupportService.format(responseDTO.getResponseBody()));
-                    statusIndicator = (responseDTO.getStatusCode() == 200); // TODO RZ: move to constants
+    public void savingResultHistory(Request request, ResultHistory resultHistory, RequestDTO requestDTO,
+            ResultsDTO resultsDTO, List<ResponseDTO> responseDTOList, int collectionId,
+            DbValidationHistory dbValidationHistory) {
+        
+        Environment environment = resultsDTO.getEnvironment();
+        final int SUCCESS_CODE = 200;
+        boolean statusIndicator = false;
+        int buildVersionId = resultsDTO.getBuildVersionId();
+
+        if (buildVersionId != 0) {
+            int count = 0;
+            int timeSum = 0;
+            for (ResponseDTO responseDTOListElement : responseDTOList) {
+                if (responseDTOListElement.getStatusCode() == SUCCESS_CODE
+                        && true /*
+                                 * TODO AM: check if actual response is equal to
+                                 * expected + other validation
+                                 */) {
+                    timeSum += responseDTOListElement.getResponseTime();
+                    count++;
+                    resultHistory.setStatusLine(responseDTOListElement.getStatusLine());
+                    resultHistory.setActualResponse(
+                            requestExecuteSupportService.format(responseDTOListElement.getResponseBody()));
                 }
+                resultHistory.setResponseTime(timeSum / count);
+                statusIndicator = (count == 5); // TODO RZ: move to
+                                                // constants use from
+                                                // property file, which
+                                                // Anton should extract
+            }
+        } else {
+            ResponseDTO responseDTO = responseDTOList.get(0);
+            resultHistory.setResponseTime(responseDTO.getResponseTime());
+            resultHistory.setStatusLine(responseDTO.getStatusLine());
+            resultHistory.setActualResponse(requestExecuteSupportService.format(responseDTO.getResponseBody()));
+            statusIndicator = (responseDTO.getStatusCode() == SUCCESS_CODE); 
+        }
+        
+        resultHistory.setApplication(request.getApplication());
+        resultHistory.setService(request.getService());
+        resultHistory.setRequest(request);
+        resultHistory.setRequestName(request.getName());
+        resultHistory.setRequestDescription(request.getDescription());
+        resultHistory.setUrl(requestDTO.getHttpRequest().getURI().toString());
+        resultHistory.setResponseType(request.getResponseType().getTextValue());
+        resultHistory.setTimeStart(new Timestamp(System.currentTimeMillis()));
+        resultHistory.setExpectedResponseTime(request.getTimeout());
 
-                // RESULT_HISTORY
+        try {
+            if ((requestDTO.getHttpRequest().getMethod().equals("GET"))
+                    ^ (requestDTO.getHttpRequest().getMethod().equals("DELETE"))) {
+                resultHistory.setRequestBody(null);
+            } else {
+                resultHistory.setRequestBody(EntityUtils
+                        .toString(((HttpEntityEnclosingRequestBase) requestDTO.getHttpRequest()).getEntity()));
+            }
 
-                resultHistory.setApplication(request.getApplication());
-                resultHistory.setService(request.getService());
-                resultHistory.setRequest(request);
-                resultHistory.setRequestName(request.getName());
-                resultHistory.setRequestDescription(request.getDescription());
-                resultHistory.setUrl(requestDTO.getHttpRequest().getURI().toString());
-                resultHistory.setResponseType(request.getResponseType().getTextValue());
-                resultHistory.setTimeStart(new Timestamp(System.currentTimeMillis()));
-                resultHistory.setExpectedResponseTime(request.getTimeout());
+            resultHistory.setExpectedResponse(requestExecuteSupportService
+                    .getEvaluatedString(request.getExpectedResponse(), requestDTO.getVariableList(), VELOCITY_LOG));
 
+        } catch (ParseException | IOException e1) {
+            LOGGER.info(e1);
+        }
+
+        resultHistory.setRunId(resultsDTO.getRunId());
+
+        if (collectionId != 0) {
+            RequestCollection reqColl = requestCollectionService.load(collectionId);
+            resultHistory.setRequestCollection(reqColl);
+        }
+        if (buildVersionId != 0) {
+            BuildVersion bVersion = metaDataService.loadBuildVersionById(buildVersionId);
+            resultHistory.setBuildVersion(bVersion);
+        }
+
+        StringBuilder validationMessages = getValidationMessages(resultHistory, request, requestDTO, dbValidationHistory);
+        resultHistory.setStatus(statusIndicator && isValidResponse(validationMessages));
+        resultHistory.setMessage(validationMessages.toString());
+        resultHistoryService.save(resultHistory);
+        savingDbValidation(request, environment, resultHistory, dbValidationHistory);
+    }
+
+    public void savingEnvironmentHistory(EnvironmentHistory environmentHistory, ResultHistory resultHistory,
+            ResultsDTO resultsDTO) {
+
+        Environment environment = resultsDTO.getEnvironment();
+
+        environmentHistory.setResultHistory(resultHistory);
+        environmentHistory.setBaseURL(environment.getBaseUrl());
+        environmentHistory.setDbName(environment.getDbName());
+        environmentHistory.setDbPort(environment.getDbPort());
+        environmentHistory.setDbURL(environment.getDbUrl());
+        environmentHistory.setName(environment.getName());
+        environmentHistory.setEnvironment(environment);
+        LOGGER.info("ENVIRONMENT " + environmentHistory);
+        resultHistoryService.saveEnvironmentHistory(environmentHistory);
+
+    }
+
+    public void savingHeaderHistory(Request request, HeaderHistory headerHistory, ResultHistory resultHistory) {
+
+        if (request.getHeaders() != null) {
+            List<Header> headers = request.getHeaders();
+            for (Header header : headers) {
+                headerHistory.setName(header.getName());
+                headerHistory.setValue(header.getValue());
+                headerHistory.setResultHistory(resultHistory);
+                LOGGER.info("HEADER_HISTORY " + headerHistory);
+                resultHistoryService.saveHeaderHistory(headerHistory);
+            }
+        }
+    }
+
+    public void savingLabelHistory(Request request, ResultHistory resultHistory) {
+
+        List<Label> labels = (request.getLabels());
+        resultHistory.setLabels(labels);
+        LOGGER.info("LABELS " + resultHistory.getLabels());
+        if (resultHistory.getLabels() != null) {
+            resultHistoryService.saveResultHistoryComponents(resultHistory);
+        }
+    }
+
+    public void savingDbValidation(Request request, Environment environment, ResultHistory resultHistory,
+            DbValidationHistory dbValidationHistory) {
+
+        if (CollectionUtils.isNotEmpty(request.getDbValidations())) {
+            List<DbValidation> dbValidations = (request.getDbValidations());
+            for (DbValidation dbValidation : dbValidations) {
+                dbValidationHistory.setSqlQuery(dbValidation.getSqlQuery());
+                dbValidationHistory.setExpectedValue(dbValidation.getExpectedValue());
                 try {
-                    if ((requestDTO.getHttpRequest().getMethod().equals("GET"))
-                            ^ (requestDTO.getHttpRequest().getMethod().equals("DELETE"))) {
-                        resultHistory.setRequestBody(null);
-                    } else {
-                        resultHistory.setRequestBody(EntityUtils.toString(((HttpEntityEnclosingRequestBase) requestDTO
-                                .getHttpRequest()).getEntity()));
-                    }
-
-                    resultHistory.setExpectedResponse(requestExecuteSupportService.getEvaluatedString(
-                            request.getExpectedResponse(), requestDTO.getVariableList(), VELOCITY_LOG));
-
-                } catch (ParseException | IOException e1) {
-                    LOGGER.info(e1);
+                    dbValidationHistory.setActualValue(requestExecuteSupportService.getExecutedQueryValue(environment,
+                            dbValidation.getSqlQuery()));
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    // TODO RZ: set NULL and message to the validation
+                    // messages
                 }
 
-                resultHistory.setRunId(runId);
-
-                if (collectionId != 0) {
-                    RequestCollection reqColl = requestCollectionService.load(collectionId);
-                    resultHistory.setRequestCollection(reqColl);
-                }
-                if (buildVersionId != 0) {
-                    BuildVersion bv = metaDataService.loadBuildVersionById(buildVersionId);
-                    resultHistory.setBuildVersion(bv);
-                }
-                
-              //DB-VALIDATION
-
-                if (CollectionUtils.isNotEmpty(request.getDbValidations())) {
-                    List<DbValidation> dbValidations = (request.getDbValidations());
-                    for (DbValidation dbValidation : dbValidations) {
-                        dbValidationHistory.setSqlQuery(dbValidation.getSqlQuery());
-                        dbValidationHistory.setExpectedValue(dbValidation.getExpectedValue());
-                        try {
-                            dbValidationHistory.setActualValue(requestExecuteSupportService.getExecutedQueryValue(
-                                    environment, dbValidation.getSqlQuery()));
-                        } catch (Exception e) {
-                            // TODO Auto-generated catch block
-                            e.printStackTrace();
-                            // TODO RZ: set NULL and message to the validation messages
-                        }
-
-                        LOGGER.info("DB_VALIDATION   " + dbValidationHistory);
-                        dbValidationHistory.setResultHistory(resultHistory);
-                    }
-                }
-
-                String validationMessages = getValidationMessages(resultHistory, request, requestDTO, dbValidationHistory);
-                resultHistory.setStatus(statusIndicator && isValidResponse(validationMessages));
-                resultHistory.setMessage(validationMessages);
-                resultHistoryService.save(resultHistory);
-
+                LOGGER.info("DB_VALIDATION   " + dbValidationHistory);
+                dbValidationHistory.setResultHistory(resultHistory);
                 if (CollectionUtils.isNotEmpty(request.getDbValidations())) {
                     dbValidationHistory.setResultHistory(resultHistory);
                     resultHistoryService.saveDbValidationHistory(dbValidationHistory);
                 }
-                    // ENVIRONMENT_HISTORY
-
-                environmentHistory.setResultHistory(resultHistory);
-                environmentHistory.setBaseURL(environment.getBaseUrl());
-                environmentHistory.setDbName(environment.getDbName());
-                environmentHistory.setDbPort(environment.getDbPort());
-                environmentHistory.setDbURL(environment.getDbUrl());
-                environmentHistory.setName(environment.getName());
-                environmentHistory.setEnvironment(environment);
-                LOGGER.info("ENVIRONMENT " + environmentHistory);
-                resultHistoryService.saveEnvironmentHistory(environmentHistory);
-
-                // HEADER_HISTORY
-
-                if (request.getHeaders() != null) {
-                    List<Header> headers = request.getHeaders();
-                    for (Header header : headers) {
-                        headerHistory.setName(header.getName());
-                        headerHistory.setValue(header.getValue());
-                        headerHistory.setResultHistory(resultHistory);
-                        LOGGER.info("HEADER_HISTORY " + headerHistory);
-                        resultHistoryService.saveHeaderHistory(headerHistory);
-                    }
-                }
-
-                // LABELS
-
-                List<Label> labels = (request.getLabels());
-                resultHistory.setLabels(labels);
-                LOGGER.info("LABELS " + resultHistory.getLabels());
-                if (resultHistory.getLabels() != null) {
-                    resultHistoryService.saveResultHistoryComponents(resultHistory);
-                }
             }
-
         }
 
-        return runId;
     }
 
-    private boolean isValidResponse(String validationMessages) {
-        return validationMessages.equals("OK");
+    private boolean isValidResponse(StringBuilder validationMessages) {
+        
+        return validationMessages.toString().equals("OK");
     }
 
-    public String getValidationMessages(ResultHistory resultHistory, Request request, RequestDTO requestDTO,
+    public StringBuilder getValidationMessages(ResultHistory resultHistory, Request request, RequestDTO requestDTO,
             DbValidationHistory dbValidationHistory) {
-        String message = null;
+        
+        StringBuilder message = new StringBuilder();
         String type = null;
         if (resultHistory.getExpectedResponse().startsWith("<")) {
             type = "XML";
-        } else {
+        } 
+        if (resultHistory.getExpectedResponse().startsWith(("["))
+                ||resultHistory.getExpectedResponse().startsWith(("{"))) {
             type = "JSON";
         }
-        
 
-        if (!resultHistory.getExpectedResponse().equalsIgnoreCase(resultHistory.getActualResponse())
-                && (!requestDTO.getHttpRequest().getMethod().equals("POST"))) { // TODO RZ: use Enum RequestMethod
-            message += "Actual response does not match the expected one";
-        }
-        if ((!requestDTO.getHttpRequest().getMethod().equals("GET"))
-                && (!requestDTO.getHttpRequest().getMethod().equals("POST"))) { // TODO RZ: use Enum RequestMethod, ie RequestMethod.USED.name()
-            message += "Unsupported request method";
+        if ((RequestMethod.DELETE.name().equalsIgnoreCase("DELETE"))) {
+            message.append("Unsupported request method; ");
         }
         if (!type.equalsIgnoreCase(request.getResponseType().getTextValue())) {
             LOGGER.info("TYPE " + request.getResponseType().getTextValue());
-            message += "Wrong content type";
+            message.append("Wrong content type; ");
+        }
+        if (!resultHistory.getExpectedResponse().equalsIgnoreCase(resultHistory.getActualResponse())
+                && (RequestMethod.GET.name().equalsIgnoreCase("GET"))) {
+            message.append("Actual response does not match the expected one; ");
         }
         if (resultHistory.getResponseTime() > resultHistory.getExpectedResponseTime()) {
-            message += "Response time exceeded timeout value";
+            message.append("Response time exceeded timeout value; ");
         }
 
         /*
@@ -244,7 +274,7 @@ public class ParseAndWriteService {
          */
 
         if (StringUtils.isBlank(message)) {
-            message = "OK";
+            message.append("OK");
         }
         return message;
     }
